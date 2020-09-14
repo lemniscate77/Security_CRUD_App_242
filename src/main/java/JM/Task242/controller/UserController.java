83ba07261d6bf02fdc72dc52fb040921096dfcd2
lemniscate77 <lemniscate77@users.noreply.github.com>
2020-09-14 17:46:42 +0300

    Полностью рабочая версия

--- src/main/java/JM/Task242/controller/UserController.java
+++ src/main/java/JM/Task242/controller/UserController.java
@@ -1,19 +1,20 @@
 package JM.Task242.controller;
 
 import JM.Task242.model.User;
-import JM.Task242.service.UserServiceImpl;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import JM.Task242.service.UserService;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
-    private UserServiceImpl userService;
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
 
     @GetMapping(value = "/")
@@ -30,17 +31,11 @@ public class UserController {
 
     @GetMapping(value = "/user")
     public String getUserPage(Model model) {
-         User user = (User) SecurityContextHolder
+         UserDetails user = (UserDetails) SecurityContextHolder
                 .getContext()
                 .getAuthentication()
                 .getPrincipal();
-        User user1 = userService.getByName(user.getUsername());
-        User user2 = new User();
-        user2.setId(159);
-        user2.setFirstName("USER2-NAME");
-        user2.setLastName("USER2-LASTNAME");
-        user2.setEmail("USER2@EMAIL.EMAIL");
-        user2.setPassword("USER2-PASS");
+        User user1 = userService.getUserByName(user.getUsername());
         model.addAttribute("user", user1);
         return "user-details";
     }
